335a617f49c53caf0082889078a2578ed4051ec6
Ayesha Ali <aali0016@student.monash.edu>
2024-07-28 04:57:16 +1000

    Basic modifications

--- react-nice-resume-master/react-nice-resume-master/src/Components/Contact.js
+++ react-nice-resume-master/react-nice-resume-master/src/Components/Contact.js
@@ -24,14 +24,15 @@ class Contact extends Component {
             </div>
 
             <div className="ten columns">
-              <p className="lead">{message}</p>
+                        <h3>{message}</h3>
             </div>
           </div>
         </Fade>
 
         <div className="row">
           <Slide left duration={1000}>
-            <div className="eight columns">
+                    <div className="eight columns">
+                      
               <form action="" method="post" id="contactForm" name="contactForm">
                 <fieldset>
                   <div>
@@ -103,50 +104,7 @@ class Contact extends Component {
             </div>
           </Slide>
 
-          <Slide right duration={1000}>
-            <aside className="four columns footer-widgets">
-              <div className="widget widget_contact">
-                <h4>Address and Phone</h4>
-                <p className="address">
-                  {name}
-                  <br />
-                  {street} <br />
-                  {city}, {state} {zip}
-                  <br />
-                  <span>{phone}</span>
-                </p>
-              </div>
 
-              <div className="widget widget_tweets">
-                <h4 className="widget-title">Latest Tweets</h4>
-                <ul id="twitter">
-                  <li>
-                    <span>
-                      This is Photoshop's version of Lorem Ipsum. Proin gravida
-                      nibh vel velit auctor aliquet. Aenean sollicitudin, lorem
-                      quis bibendum auctor, nisi elit consequat ipsum
-                      <a href="./">http://t.co/CGIrdxIlI3</a>
-                    </span>
-                    <b>
-                      <a href="./">2 Days Ago</a>
-                    </b>
-                  </li>
-                  <li>
-                    <span>
-                      Sed ut perspiciatis unde omnis iste natus error sit
-                      voluptatem accusantium doloremque laudantium, totam rem
-                      aperiam, eaque ipsa quae ab illo inventore veritatis et
-                      quasi
-                      <a href="./">http://t.co/CGIrdxIlI3</a>
-                    </span>
-                    <b>
-                      <a href="./">3 Days Ago</a>
-                    </b>
-                  </li>
-                </ul>
-              </div>
-            </aside>
-          </Slide>
         </div>
       </section>
     );
--- react-nice-resume-master/react-nice-resume-master/src/Components/Header.js
+++ react-nice-resume-master/react-nice-resume-master/src/Components/Header.js
@@ -6,7 +6,7 @@ class Header extends Component {
   render() {
     if (!this.props.data) return null;
 
-    const project = this.props.data.project;
+ 
     const github = this.props.data.github;
     const name = this.props.data.name;
     const description = this.props.data.description;
@@ -38,13 +38,13 @@ class Header extends Component {
 
             <li>
               <a className="smoothscroll" href="#resume">
-                Resume
+                Route Plan
               </a>
             </li>
 
             <li>
               <a className="smoothscroll" href="#portfolio">
-                Works
+                Future Work
               </a>
             </li>
 
@@ -67,11 +67,9 @@ class Header extends Component {
             <hr />
             <Fade bottom duration={2000}>
               <ul className="social">
-                <a href={project} className="button btn project-btn">
-                  <i className="fa fa-book"></i>Project
-                </a>
+
                 <a href={github} className="button btn github-btn">
-                  <i className="fa fa-github"></i>Github
+                  <i className="fa fa-github"></i>GitHub
                 </a>
               </ul>
             </Fade>
